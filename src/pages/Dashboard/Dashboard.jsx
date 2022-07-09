0d50ece7ada60ac8dd44c49d046316af73700c84
rutu14 <vidyapunjabi7@gmail.com>
2022-07-09 18:51:05 +0530

    feat: Auth Pages

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+import { Button, Heading } from "@chakra-ui/react"
+import { useNavigate } from "react-router";
+import { logOut } from "../../redux/auth";
+import { useDispatch } from "react-redux";
+
+const Dashboard = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const logOutD = () => {
+        dispatch(logOut());
+        navigate("/");
+    }
+    return(
+        <Heading fontSize={'xl'}>
+            Welcome to Dashboard
+        <Button onClick={logOutD}>Logout</Button>
+        </Heading>
+    )
+}
+
+export default Dashboard;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import { Box,Heading,Container,Text,Button,Stack} from '@chakra-ui/react';
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return(
+        <Container maxW={'3xl'}>
+            <Stack as={Box} textAlign={'center'} spacing={{ base: 8, md: 14 }} py={{ base: 20, md: 36 }}>
+                <Heading fontWeight={600} fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }} lineHeight={'110%'}>
+                    Pen down your <></>
+                    <Text as={'span'} color={'green.400'}>thoughts</Text>
+                </Heading>
+                <Text fontSize={'2xl'} color={'gray.500'}>
+                    Inspiration strikes anywhere. Create organized and creative notes anytime, anywhere.
+                    Effortless, Organised & Share.
+                </Text>
+                <Stack direction={'column'} spacing={3} align={'center'} alignSelf={'center'} position={'relative'}>
+                    <Button as={Link} to={'/login'} size='lg' fontSize={'md'} colorScheme={'green'} bg={'green.400'} rounded={'full'} px={6} _hover={{bg: 'green.500',}}>
+                        Get Started
+                    </Button>
+                </Stack>
+            </Stack>
+      </Container>
+    );
+}
+
+export default Home;
